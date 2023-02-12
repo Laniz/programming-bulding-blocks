d8e0f10430038cfbb2fc48a84b7aaefd1494a4d6
laniz <shdrocks.ncube@gmail.com>
2023-02-11 22:00:22 -0700

    more changes

--- Week 5/adventure_game.py
+++ Week 5/adventure_game.py
@@ -1,13 +1,13 @@
-a00 = 'Game will now end. Wrong input'
+a00 = 'Game will now end. Wrong input!'
 
 a1 = '''The cold wakes you up, slowly you open your eyes. You pull out your phone, enter the 
 password "open sesame". As soon as you do, your phone dies. Realizing you are all alone in the dark, do you SCREAM for 
 help or CRAWL around the cave. '''
 
-a2 = '''\nYou scream as loud as you can, A familiar voices calls back
-to you. Relief sweeps over you, knowing you can FOLLOW the voice, but maybe SEARCH fo supplies first? '''
+a2 = '''\nYou scream as loud as you can, A familiar voices calls back to you. Relief sweeps
+ over you, knowing you can FOLLOW the voice, but maybe SEARCH fo supplies first? '''
 
-a3 = '''\nYou follow the voice, the cave seems to narrow, still in the dark.
+a3 = '''\nYou follow the voice, the cave seems to narrow, still in the dark. 
 CONTINUE on this narrowing path or RETURN where you were? '''
 
 a4  = '''\nthe cave suddedly widdens, light blinding your eyes. You have been found and rescured, 
@@ -15,16 +15,16 @@ the joy wakes you up from your dream '''
 
 a5 = '\nAs you turn around, you feel the ground under you give way. The hypnic jerk immediately wakes you up'
 
-a6 = '''\nYou feel around in the dark, there is flashlight right next you. Do 
-you TURN ON the flash light or search for MORE? '''
+a6 = '''\nYou feel around in the dark, there is flashlight right next you. Do you TURN ON
+ the flash light or search for MORE? '''
 
 a7 = '''\nLuckly you find a door. Some how the door speaks. Says "Silver is the only passage, 
 you know the ancient password". "What password!?" You yell. You feel around the door, it feels flimsy enough to be 
-forced open. What do you? enter the PASSWORD, PAY, Or BREAK the door?   '''
+forced open. What do you do? enter the PASSWORD, PAY, Or BREAK the door? '''
 
 a8 = '"What is the password!" the door roars. '
 
-a9 = '"What is the password of the of the ancients, beg for MERCY if you do not know!" '
+a9 = '"What is the password of the ancients, beg for MERCY if you do not know!" '
 
 a10 = '''"I offer no mercy to mortals!" the door screams. Your alarm rings, you wake up and as 
 you unlock your phone you realise that the password was open sesame.'''
@@ -38,19 +38,24 @@ a13 = '\nJust kidding you have failed no more attempts left! The door ends your 
 
 a14 = 'Enter the amount you want to pay. Pay with SILVER or Nothing '
 
-a15 = 'the door accecpts your payment, unfortunately the birds outside wake up before you see the ending'
+a15 = '''the door accecpts your payment, unfortunately the birds outside wake up before you
+ see the ending'''
 
-a16 = 'You empty out your pockets, you find 10 sliver coins. You pay the door and it opens up as you wake up, questioning reality itself'
+a16 = '''You empty out your pockets, you find 10 sliver coins. You pay the door and it opens up as 
+you wake up, questioning reality itself'''
 
 a17 = 'the door disappears, you sit in darkness, contepting the finiteness of life, But your alarm saves you '
 
-a18 = 'you struggle agaisnt the door, Your strength impresses the Door. It asks you stop before it lets you through, Do you STOP or Not'
+a18 = '''you struggle against the door, Your strength impresses the Door. It asks you stop before it lets you 
+through, Do you STOP or NOT'''
 
 a19 = 'The door calls you a worthy mortal, and it lets you through. You sleep blissfully till your alarm goes off'
 
-a20 = 'The harder you push against door the studier it comes. With all in effort, you were slowly slidding across the bed, till eventually you are woken up by the fall'
+a20 = '''The harder you push against door the studier it becomes. With all that effort, you were slowly slidding across
+ the bed, till eventually you are woken up by the fall'''
 
-a21 = 'Light fills the room. You can clearly see a door markerd "Exit". "it cant be that easy" you say to yourself. But yes it is that easy. Exiting wakes you up'
+a21 = '''Light fills the room. You can clearly see a door markerd "Exit". "it cant be that easy" you say to yourself. 
+But yes it is that easy. Exiting wakes you up'''
 
 a22 = 'You find a button that says, "Push to exit" and thats what you do. And you wake up disappointed.'
 
@@ -109,7 +114,6 @@ elif first_opt.lower() == 'crawl':
             print(' ')
             password = input(a9)
 
-            #password will only work once countdown has started, to try and trick the user
             if count == 4:
                 print('this is your last attempt!! ')
 
@@ -130,30 +134,47 @@ elif first_opt.lower() == 'crawl':
                 print(f'''As you head towards the door on your {last_in.lower()}, you wake up because
 your body is used to waking up a few minutes before your alarm rings \n''')
                 break
+        if first_opt_br_two_one.lower() == 'open sesame':
+            print(' ')
+            last_in = input(a11)
+            print(' ')
+            print(f'''As you head towards the door on your {last_in.lower()}, you wake up because
+your body is used to waking up a few minutes before your alarm rings \n''')
+        # else:
+        #     print(' ')
+        #     print(a00)
+
+
 
 
     #Pay chosen br 2 lvl 1 two
     elif first_opt_br_two.lower() == 'pay':
         print(' ')
         first_opt_br_two_two = input(a14)
+
         if first_opt_br_two_two.lower() == 'silver':
             print(' ')
             print(a16)
+
         elif first_opt_br_two_two.lower() == 'nothing':
             print(' ')
             print(a17)
+
         else:
             print(' ')
             print(a00)
+
     elif first_opt_br_two.lower() == 'break':
         too_sleepy_to_think_of_a_variable_name = (a18)
 
         if too_sleepy_to_think_of_a_variable_name.lower() == 'stop':
             print(' ')
             print(a19)
+
         elif too_sleepy_to_think_of_a_variable_name.lower() == 'not':
             print(' ')
             print(a20)
+            
         else:
             print(' ')
             print(a00)
@@ -162,6 +183,7 @@ your body is used to waking up a few minutes before your alarm rings \n''')
         print(a00)
 
 else:
+    print(' ')
     print(a00)
 
 
