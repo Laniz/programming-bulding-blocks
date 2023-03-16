0f399a16bd62a7a2b4e3fe5d227c9714864b0cfa
laniz <shdrocks.ncube@gmail.com>
2023-03-16 09:20:45 -0600

    aaaa

---
+++ Week 11/fruits.py
@@ -0,0 +1,18 @@
+# with open('fruits.txt') as fruit_file:
+#     for line in fruit_file:
+#         line = line.strip()
+#         fruit_data = line.split(',')
+
+#         fruit_name = fruit_data[0]
+#         fruit_cost = float(fruit_data[2])
+
+#         print(f'{fruit_name} costs ${fruit_cost:.2f} per pound')
+
+
+
+
+line= '    text'
+
+line.strip()
+
+print(line)
